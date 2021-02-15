3ce431364eb77e2d95a4459fa99bbd7ad818ec4d
Marwil96 <william_martinsson@hotmail.com>
2021-02-15 15:29:42 +0100

    now fetches all imgages and portrait images

--- index.py
+++ index.py
@@ -1,56 +1,74 @@
 from wikitables import import_tables
 import wikipedia
 import json
+import requests
 print("Hello, world!")
 
 allDriversTable = import_tables('List of Formula One drivers')
 
+WIKI_REQUEST = 'http://en.wikipedia.org/w/api.php?action=query&prop=pageimages&format=json&piprop=original&titles='
+
+
+def get_wiki_image(search_term):
+    try:
+        result = wikipedia.search(search_term, results=1)
+        wikipedia.set_lang('en')
+        wkpage = wikipedia.WikipediaPage(title=result[0])
+        title = wkpage.title
+        response = requests.get(WIKI_REQUEST+title)
+        json_data = json.loads(response.text)
+        img_link = list(json_data['query']['pages'].values())[
+            0]['original']['source']
+        return img_link
+    except:
+        return 0
+
+
 drivers = []
 def show_todo():
    for row in allDriversTable[1].rows:
-    drivers.append(( {
-      'name':'{Driver Name}'.format(**row).replace(' ~', '').replace(' ^','').replace(' *', ''),
-      'nationality': '{Nationality}'.format(**row),
-      'seasons_completed': '{Seasons Competed}'.format(**row),
-      'drivers_championships':"{Drivers' Championships}".format(**row),
-      'race_entries': '{Race Entries}'.format(**row),
-      'race_starts': '{Race Starts}'.format(**row),
-      'pole_positions': '{Pole Positions}'.format(**row),
-      'race_wins': '{Race Wins}'.format(**row),
-      'podiums': '{Podiums}'.format(**row),
-      'fastest_laps': '{Fastest laps}'.format(**row),
-      'points': '{Points}'.format(**row),
-    }))
+      drivers.append(( {
+        'name':'{Driver Name}'.format(**row).replace(' ~', '').replace(' ^','').replace(' *', ''),
+        'nationality': '{Nationality}'.format(**row),
+        'seasons_completed': '{Seasons Competed}'.format(**row),
+        'drivers_championships':"{Drivers' Championships}".format(**row),
+        'race_entries': '{Race Entries}'.format(**row),
+        'race_starts': '{Race Starts}'.format(**row),
+        'pole_positions': '{Pole Positions}'.format(**row),
+        'race_wins': '{Race Wins}'.format(**row),
+        'podiums': '{Podiums}'.format(**row),
+        'fastest_laps': '{Fastest laps}'.format(**row),
+        'points': '{Points}'.format(**row),
+      }))
 
 a = show_todo()
 
 for driver in drivers:
   # driverData = wikipedia.page(wikipedia.search('{name}'.format(**driver))[0])
-  print(driver)
   try:
     # driverData = wikipedia.page(wikipedia.search('{name}'.format(**driver))[0])
     driverData = wikipedia.WikipediaPage('{name}'.format(**driver))
     print(driverData.images[0], '{name}'.format(**driver) )
     driverDetails = import_tables('{name}'.format(**driver))
-    driver['portrait_image'] = driverData.images[0]
+    wiki_image = get_wiki_image('{name}'.format(**driver))
+    driver['portrait_image'] = wiki_image
     driverSeason = []
     for detailsRow in driverDetails[0].rows:
       if '{Series}'.format(**detailsRow) == 'Formula One':
-        print('{Team}'.format(**detailsRow))
         driverSeason.append(({
           'season':'{Season}'.format(**detailsRow),
           'team':'{Team}'.format(**detailsRow),
           'races':'{Races}'.format(**detailsRow),
           'wins':'{Wins}'.format(**detailsRow),
           'poles':'{Poles}'.format(**detailsRow),
-          'fastest_laps':'{Flaps}'.format(**detailsRow),
+          'fastest_laps':'{FLaps}'.format(**detailsRow),
           'podiums':'{Podiums}'.format(**detailsRow),
           'points':'{Points}'.format(**detailsRow),
           'position':'{Position}'.format(**detailsRow),
-
           }))
 
-    driver['drive'] = driverData.images[0]
+    print(driverSeason)
+    driver['driver_images'] = driverData.images
     driver['driver_seasons'] = driverSeason
 
   except:
